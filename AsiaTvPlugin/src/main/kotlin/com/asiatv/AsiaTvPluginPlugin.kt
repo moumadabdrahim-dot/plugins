7ba467286490b91aeec24ec486330cc94fc74285
moumadabdrahim-dot <moumadabdrahim@gmail.com>
2026-09-08 14:35:58 +0100

    Update AsiaTvPluginPlugin.kt

--- AsiaTvPlugin/src/main/kotlin/com/asiatv/AsiaTvPluginPlugin.kt
+++ AsiaTvPlugin/src/main/kotlin/com/asiatv/AsiaTvPluginPlugin.kt
@@ -1,9 +1,10 @@
 package com.asiatv
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class AsiaTvPluginPlugin : CloudstreamPlugin() {
+class AsiaTvPluginPlugin : BasePlugin() {
     override fun load() {
         registerMainAPI(AsiaTvPlugin())
     }
