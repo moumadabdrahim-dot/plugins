0802062f2566bcdd2d55da8fdae49ae832b21d05
moumadabdrahim-dot <moumadabdrahim@gmail.com>
2026-09-08 14:43:57 +0100

    Update build.gradle.kts

--- AsiaTvPlugin/build.gradle.kts
+++ AsiaTvPlugin/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
     val cloudstream by configurations
+    val compileOnly by configurations
+
     cloudstream("com.lagradost:cloudstream3:pre-release")
+    compileOnly("org.jspecify:jspecify:1.0.0")
 }
