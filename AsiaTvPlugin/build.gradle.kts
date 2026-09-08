b161c802420c0127ac477cf8e6b94b547eb937f8
moumadabdrahim-dot <moumadabdrahim@gmail.com>
2026-09-08 05:31:07 +0100

    Refactor build.gradle.kts for CloudStream plugin setup

--- AsiaTvPlugin/build.gradle.kts
+++ AsiaTvPlugin/build.gradle.kts
@@ -1,5 +1,113 @@
-// CloudStream dependency is provided by the CloudStream TestPlugins template.
+import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.gradle.kotlin.dsl.register
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
-dependencies {
-    implementation("org.jspecify:jspecify:1.0.0")
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            force("com.github.vidstige:jadb:v1.2.1")
+        }
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.github.recloudstream:gradle:32895aedb6366f5075cb99bbd2e6ce0a7cac325d")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+fun Project.cloudstream(
+    configuration: CloudstreamExtension.() -> Unit
+) = extensions
+    .getByName<CloudstreamExtension>("cloudstream")
+    .configuration()
+
+fun Project.android(
+    configuration: BaseExtension.() -> Unit
+) = extensions
+    .getByName<BaseExtension>("android")
+    .configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "moumadabdrahim-dot/plugins"
+        )
+        authors = listOf("moumadabdrahim")
+    }
+
+    android {
+        namespace = "com.moumadabdrahim"
+
+        defaultConfig {
+            minSdk = 21
+            compileSdkVersion(37)
+            targetSdk = 36
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+
+        tasks.withType<KotlinJvmCompile> {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_1_8)
+
+                freeCompilerArgs.addAll(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
+            }
+        }
+    }
+
+    dependencies {
+        val implementation by configurations
+        val cloudstream by configurations
+
+        cloudstream(
+            "com.lagradost:cloudstream3:pre-release"
+        )
+
+        implementation(kotlin("stdlib"))
+
+        implementation(
+            "com.github.Blatzar:NiceHttp:0.4.18"
+        )
+
+        implementation(
+            "org.jsoup:jsoup:1.22.1"
+        )
+
+        implementation(
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
+        )
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
